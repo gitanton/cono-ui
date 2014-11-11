7559a388bd606fd59988a48590e95744632c97d0
Susie wenjia Chen <chenwj@shinetechchina.com>
2014-11-11 22:36:35 +0800

    [the template for "add new message,delete message,reply message"]

--- www/app/scripts/controllers/message.js
+++ www/app/scripts/controllers/message.js
@@ -9,9 +9,29 @@
  */
 angular.module('conojoApp')
   .controller('MessageCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    $scope.openAddNewMessage = function(){
+        $('#addNewMessage').modal('toggle');
+        $('body').css('padding',0);
+    };
+    
+    $scope.replyMessageModal = function(){
+        $('#replymessage').modal('toggle');
+        $('body').css('padding',0);
+    };
+    
+    $scope.deleteMessageModal = function(){
+        $('#deletemessage').modal('toggle');
+        $('body').css('padding',0);
+    };
+    
+    $scope.deleteMessage = function(uuid){
+        $http({
+            url: 'http://conojoapp.scmreview.com/rest/projects/project/'+uuid,
+            method: 'DELETE',
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+        }).success(function() {
+            $scope.init();
+            $('#deletemessage').modal('hide');
+        });
+    };
   });
