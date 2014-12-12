0aa3a7a32a5bd52015cfb79160cc6e82fc7602e6
Susie wenjia Chen <chenwj@shinetechchina.com>
2014-12-12 22:08:18 +0800

    [drag and drop the scrren to upload,add dragging and dropping the activity item to delete]

--- www/app/scripts/controllers/project-screen.js
+++ www/app/scripts/controllers/project-screen.js
@@ -17,6 +17,73 @@ angular.module('conojoApp')
         $(".siderbar-closed-container").css('height',$scope.siderbarContainer);
         $(".siderbar-closed-container-expand").css('padding-top',$scope.siderbarExpand);
         $(".projectScreen-content-body").css('height',$scope.projectScreenBody);
+        
+        	
+        // Makes sure the dataTransfer information is sent when we
+        // Drop the item in the drop box.
+        jQuery.event.props.push('dataTransfer');
+
+        // The number of images to display
+        var errMessage = 0;
+
+        // Get all of the data URIs and put them in an array
+        var dataArray = [];
+
+        // Bind the drop event to the dropzone.
+        $('#drop-files').bind('drop', function(e) {
+                // Stop the default action, which is to redirect the page
+                // To the dropped file
+                var files = e.dataTransfer.files;
+
+                // For each file
+                $.each(files, function(index, file) {
+
+                        // Some error messaging
+                        if (!files[index].type.match('image.*')) {
+
+                                if(errMessage == 0) {
+                                        $('#drop-files').html('Hey! Images only');
+                                        ++errMessage
+                                }
+                                else if(errMessage == 1) {
+                                        $('#drop-files').html('Stop it! Images only!');
+                                        ++errMessage
+                                }
+                                else if(errMessage == 2) {
+                                        $('#drop-files').html("Can't you read?! Images only!");
+                                        ++errMessage
+                                }
+                                else if(errMessage == 3) {
+                                        $('#drop-files').html("Fine! Keep dropping non-images.");
+                                        errMessage = 0;
+                                }
+                                return false;
+                        }
+                        // Start a new instance of FileReader
+                        var fileReader = new FileReader();
+                                // When the filereader loads initiate a function
+                                fileReader.onload = (function(file) {
+                                        return function(e) { 
+                                                // Push the data URI into an array
+                                                dataArray.push({name : file.name, value : this.result});
+                                                console.log(this.result);
+                                        }; 
+                                })(files[index]);
+                        // For data URI purposes
+                        fileReader.readAsDataURL(file);
+                });
+        });
+
+        // Just some styling for the drop file container.
+        $('#drop-files').bind('dragenter', function() {
+                $(this).css({'box-shadow' : 'inset 0px 0px 20px rgba(0, 0, 0, 0.1)', 'border' : '4px dashed #bb2b2b'});
+                return false;
+        });
+
+        $('#drop-files').bind('drop', function() {
+                $(this).css({'box-shadow' : 'none', 'border' : '4px dashed #fff'});
+                return false;
+        });
     };
     
     $scope.init = function(){
