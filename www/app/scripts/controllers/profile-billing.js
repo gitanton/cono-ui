766100a164d16c9892ea2d152066f13fd50c19b6
Susie wenjia Chen <chenwj@shinetechchina.com>
2015-01-28 23:13:01 +0800

    [add card info page and select plan page]

--- www/app/scripts/controllers/profile-billing.js
+++ www/app/scripts/controllers/profile-billing.js
@@ -9,13 +9,35 @@
  */
 angular.module('conojoApp')
   .controller('ProfileBillingCtrl', function ($scope) {
+     $scope.billingOne = true;
+     $scope.billingTwo = false;
+     $scope.billingThree = false;
+      
      $scope.setHeight = function(){
         $scope.siderbarContainer = $(window).height() - 64;
         $scope.siderbarExpand = $(window).height() - 442;
-        $scope.profileProjectContent = $(window).height() - 128;
+        $scope.profileBillingContent = $(window).height() - 250;
         
         $(".siderbar-closed-container").css('height',$scope.siderbarContainer);
         $(".siderbar-closed-container-expand").css('padding-top',$scope.siderbarExpand);
-        $(".profileProject-content").css('height',$scope.profileProjectContent);
+        $(".profileBilling-content-billing").css('height',$scope.profileBillingContent);
     };
+    
+    $scope.backBilling = function(){
+        $scope.billingOne = true;
+        $scope.billingTwo = false;
+        $scope.billingThree = false;
+    }
+    
+    $scope.changeCard = function(){
+        $scope.billingOne = false;
+        $scope.billingTwo = true;
+        $scope.billingThree = false;
+    }
+    
+    $scope.upgradePlan = function(){
+        $scope.billingOne = false;
+        $scope.billingTwo = false;
+        $scope.billingThree = true;
+    }
   });
