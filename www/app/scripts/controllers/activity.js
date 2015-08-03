1000cc95b2bf61f8b0cf98c32ff90cf246d5fced
WenjiaChen <chenwj@shinetechchina.com>
2015-08-03 20:28:04 +0800

    add activities and member's avatar

--- www/app/scripts/controllers/activity.js
+++ www/app/scripts/controllers/activity.js
@@ -8,9 +8,21 @@
  * Controller of the conojoApp
  */
 angular.module('conojoApp')
-    .controller('ActivityCtrl', function ($scope) {
+    .controller('ActivityCtrl', function ($scope, $http, ENV) {
         $scope.activityContent = $(window).height() - 128;
         $scope.activityDeleteContainer = $(window).height() - 180;
         $('.activity-content').css('height', $scope.activityContent);
         $('.activity-content-delete').css('height', $scope.activityDeleteContainer);
+
+        $scope.init = function(){
+        	$http({
+                url: ENV.API_ENDPOINT + 'activities/user',
+                method: 'GET',
+                headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+            }).success(function (data) {
+                $scope.projectActivities = data;
+            });
+        };
+
+        $scope.init();
     });
