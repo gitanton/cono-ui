2d975c07b5bca8a15df34ee0d7d990c3aab9a5f7
Peter Holcomb <peter@halfslide.com>
2015-10-05 16:47:13 -0500

    Task 3649881 Remember Me Option is not working.

--- www/app/scripts/controllers/main.js
+++ www/app/scripts/controllers/main.js
@@ -8,15 +8,31 @@
  * Controller of the conojoApp
  */
 angular.module('conojoApp')
-    .controller('MainCtrl', function ($scope, $http, $location, ENV, $window, userService) {
+    .controller('MainCtrl', function ($scope, $http, $location, ENV, store, userService) {
         $scope.loginPadding = ($(window).height() - 536) / 2;
+        $scope.rememberMe = store.get('rememberMe');
+        $scope.formData = {
+            username: store.get('username')
+        };
+
+        /**
+         * Watch the remember me value and store it in local storage
+         */
+        $scope.$watch('rememberMe', function(newVal) {
+            store.set('rememberMe', newVal);
+        });
 
         $('.login-logo').css('padding-top', $scope.loginPadding);
         $('.login-link').css('padding-bottom', $scope.loginPadding);
 
-        $scope.formData = {};
+
         $scope.processForm = function () {
             userService.login($scope.formData.username, $scope.formData.password).then(function () {
+                if($scope.rememberMe) {
+                    store.set('username', $scope.formData.username);
+                } else {
+                    store.remove('username');
+                }
                 $location.path('project');
             }, function () {
                 $('#loginNote').modal('toggle');
