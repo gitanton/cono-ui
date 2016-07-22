4e5fea773a8eb98396c80f5c03d01dd2ab64edb9
Adonis <adonis.j0101@gmail.com>
2016-07-22 17:36:19 +0800

    Added Color Palette

--- src/scripts/controllers/drawing.js
+++ src/scripts/controllers/drawing.js
@@ -10,8 +10,13 @@
 angular.module('conojoApp')
     .controller('DrawingCtrl', function ($rootScope, $scope, $http, ENV, NAV) {
 
+        // Drawing Variables
+        $scope.tool = 1;
+        $scope.color = "";
+        
+
         $scope.init = function () {
-            console.log("abcd")
+            
         };
 
         $scope.init();
